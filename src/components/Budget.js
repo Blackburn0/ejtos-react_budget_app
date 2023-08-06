099a8beadfe7fdcae151b0a5500511dc43629977
Blackburn0 <hamzatadebayo5@gmail.com>
2023-08-06 03:41:14 -0400

    Files updated

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,52 @@
-
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget,dispatch,remaining } = useContext(AppContext);
+
+    const [name] = useState('');
+    const [cost, setCost] = useState('');
+    const [action] = useState('');
+
+    const submitEvent = () => {
+
+            if(cost > remaining) {
+                alert("The value cannot exceed remaining funds  £"+remaining);
+                setCost("");
+                return;
+            }
+
+        const expense = {
+            name: name,
+            cost: parseInt(cost),
+        };
+        if(action === "Reduce") {
+            dispatch({
+                type: 'RED_EXPENSE',
+                payload: expense,
+            });
+        } else {
+                dispatch({
+                    type: 'ADD_EXPENSE',
+                    payload: expense,
+                });
+            }
+    };
     return (
         <div className='alert alert-secondary'>
             <span>Budget: £{budget}</span>
+
+            <input
+                required='required'
+                type='number'
+                id='cost'
+                value={cost}
+                style={{ marginLeft: '2rem' , size: 10}}
+                onChange={(event) => setCost(event.target.value)}>
+                </input>
+
+                <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
+                        Save
+                </button>
         </div>
     );
 };
